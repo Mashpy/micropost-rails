d3176fbba178d9ad6b3ec1c1213db3d8364ec0a5
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2013-07-07 10:21:38 +0600

    inial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MicropostRails::Application.config.secret_key_base = 'e1c78f972e2e5231dd9bffb18bfd6618ad0f0b40bfcaf8b7890b72561b394934412e8012d5cf68cff7088ab8925a3cdf99a9b9400ffe0cffe4358387a74a8698'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MicropostRails::Application.config.session_store :cookie_store, key: '_micropost-rails_session'
